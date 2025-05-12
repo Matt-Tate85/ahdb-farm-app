022c6addf5a9671d0c2b330be612432eb3e07d99
Matt-Tate85 <matt.tate@ahdb.org.uk>
2025-05-12 18:56:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,14 @@
-/* src/index.css */
+// src/index.js - Correct JavaScript file
 
-/* Global reset and base styles */
-body {
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// Temporarily comment out the CSS import until we fix the file
+// import './index.css';
+import App from './App';
 
-code {
-  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-    monospace;
-}
-
-/* App container styles */
-.app {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-
-/* Basic utility classes */
-.container {
-  width: 100%;
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 0 1rem;
-}
-
-/* Responsive adjustments */
-@media (max-width: 768px) {
-  .container {
-    padding: 0 0.5rem;
-  }
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

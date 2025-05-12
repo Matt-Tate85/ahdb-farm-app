d2f3bebe6a94b6753deb66a0e58714fac01807a8
Matt-Tate85 <matt.tate@ahdb.org.uk>
2025-05-12 18:16:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
-// src/App.js - Step 1: Adding basic routing
+// src/App.js - Direct component render without routing
 
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import FieldCheck from './pages/FieldCheck';
-import Market from './pages/Market';
-import Events from './pages/Events';
-import Library from './pages/Library';
-import Tools from './pages/Tools';
-import Settings from './pages/Settings';
-import MyFarm from './pages/MyFarm';
-import Farmbench from './pages/Farmbench';
-import KnowledgeLibrary from './pages/KnowledgeLibrary';
-import ResearchProjects from './pages/ResearchProjects';
-import MonitorFarm from './pages/MonitorFarm';
 
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
-        <div className="container mx-auto px-4">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/field-check" element={<FieldCheck />} />
-            <Route path="/market" element={<Market />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/library" element={<Library />} />
-            <Route path="/tools" element={<Tools />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/my-farm" element={<MyFarm />} />
-            <Route path="/farmbench" element={<Farmbench />} />
-            <Route path="/knowledge-library" element={<KnowledgeLibrary />} />
-            <Route path="/research-projects" element={<ResearchProjects />} />
-            <Route path="/monitor-farm" element={<MonitorFarm />} />
-            <Route path="*" element={<Dashboard />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <div className="container mx-auto px-4">
+        <h1 className="text-xl font-bold p-4">AHDB FarmAssist</h1>
+        <Dashboard />
+      </div>
     </div>
   );
 }
